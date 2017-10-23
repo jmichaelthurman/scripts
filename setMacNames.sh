626d5f967f90c826a07d62272e5edcea4cf9a5b0
jmichaelthurman <jmt@jmichaelthurman.com>
2017-10-22 22:07:59 -0500

    Update setMacNames.sh
    Added comments to clarify usage.

--- setMacNames.sh
+++ setMacNames.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-base="WGL-"
-tld="wegolook.com"
+#
+# Usage: macOs 
+#
+# The base is prepended to the system serial number to form a complete name. 
+# If you are also binding to Active Directory, remember that AD hostnames have a 
+# character limit. If your base + serial exceeds that limit, the hostname will be
+# truncated by AD. 
+#
+#
+base="<set your base here>"
+#Set your tld and uncomment, if desired. 
+#tld="<set your tld>"
+tld="local"
+#
+#get system serial number and read into var serial
 serial=ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}'
+#
+#set name in three places
+#
 /usr/sbin/scutil --set ComputerName $base$serial
 /usr/sbin/scutil --set LocalHostName $base$serial
 /usr/sbin/scutil --set HostName $base$serial.tld
